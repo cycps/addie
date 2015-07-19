dffd7395b52c700f069919278467c61a8bdc05cc
Ryan Goodfellow <rgoodfel@isi.edu>
2015-07-18 20:17:26 -0700

    starting down the path to real go code

--- design.go
+++ design.go
@@ -8,54 +8,93 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 )
 
-func main() {
+var cypdir = os.ExpandEnv("$HOME/.cypress")
+var logfile os.File
+var db *sql.DB = nil
 
-	cypdir := os.ExpandEnv("$HOME/.cypress")
-
+func initLogging() {
 	err := os.MkdirAll(cypdir+"/log", 0755)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "unable to create directory "+cypdir+"/log\n")
 		fmt.Fprintf(os.Stderr, err.Error()+"\n")
 		os.Exit(1)
 	}
-	f, err := os.OpenFile(cypdir+"/log/addie.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	logfile, err := os.OpenFile(cypdir+"/log/addie.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "unable to open "+cypdir+"/log/addie.log for writing\n")
 		fmt.Fprintf(os.Stderr, err.Error()+"\n")
 		os.Exit(1)
 	}
-	defer f.Close()
-	log.SetOutput(io.MultiWriter(f, os.Stdout))
+	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
+}
 
-	log.Printf("Cypress Design Automator .... Go!\n")
-	log.Printf("Do you know the muffin man?\n")
+func init() {
+	initLogging()
+}
 
+func exit(exitVal int) {
+	log.Println("addie shutting down")
+	logfile.Close()
+	if r := recover(); r != nil {
+		log.Println("shutdown is due to panic, panic info follows")
+		panic(r)
+	}
+	os.Exit(exitVal)
+}
+
+func catchSignals() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			log.Println("Caught intterrupt signal, cleaning up and shutting down")
+			exit(1)
+		}
+	}()
+}
+
+func dbConnect() error {
 	log.Printf("Opening connecton to pgdb\n")
-	db, err := sql.Open("postgres", "postgres://postgres@192.168.1.201/cyp?sslmode=require")
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres@192.168.1.201/cyp?sslmode=require")
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		log.Println(err)
+		return err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		log.Println(err)
+		return err
 	}
 
+	return nil
+}
+
+func dbStats() error {
+	log.Println("Cypress DB stats:")
 	rows, err := db.Query("SELECT relname, n_live_tup FROM pg_stat_user_tables")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 	defer rows.Close()
+
 	for rows.Next() {
 		var relname string
 		var n_live_tup int
 		if err := rows.Scan(&relname, &n_live_tup); err != nil {
-			log.Fatal(err)
-			os.Exit(1)
+			log.Println(err)
 		}
 		log.Printf("%s {%d}", relname, n_live_tup)
 	}
+	return err
+}
+
+func handleRequests() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
@@ -70,5 +109,22 @@ func main() {
 	})
 	log.Println("listening ...")
 	http.ListenAndServe(":8080", mux)
+
+}
+
+func main() {
+
+	defer exit(0)
+	catchSignals()
+
+	log.Printf("Cypress Design Automator .... Go!\n")
+	if dbConnect() != nil {
+		exit(1)
+	}
+	if dbStats() != nil {
+		exit(1)
+	}
+
+	handleRequests()
 
 }
